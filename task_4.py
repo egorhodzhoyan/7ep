be43ba105b20758d83184d1037787cb454557c2b
Egor <egor.hodzhoyan@ro.ru>
2025-03-19 20:54:50 +0300

    update

--- task_4.py
+++ task_4.py
@@ -2,6 +2,7 @@ summ = 0
 count = 0
 a = int(input('Введите число a: '))
 b = int(input('Введите число b: '))
+print('Числа из отрезка, которые делятся на 3:')
 for i in range(a, b + 1):
     if i % 3 == 0:
         print(i)
